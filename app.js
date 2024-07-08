4506aee8d6e727f833e1aa2d9105c976e129d8b6
Guitarband <preetishchoudhary@gmail.com>
2024-07-08 14:07:32 +1000

    added auth0

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cors())
-app.use(auth(config));
+app.use(auth(authConfig));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
